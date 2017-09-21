96d35b304bd7839063db9b0b8d1858dfa461b8e5
Martin <martinjmartinezcruz@gmail.com>
2017-09-21 13:37:27 -0400

    klk

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -28,7 +28,7 @@ module.exports = {
     const { powerValue } = req.value.params;
     console.log(powerValue);
     const history = await History.findById(historyId);
-    history.powerLog.push(45);
+    history.powerLog.push(powerValue);
     await history.save();
     res.status(200).json({ success: true});
   },
